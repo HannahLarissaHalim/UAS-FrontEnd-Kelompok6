9e29ef77333b400c8e7e99e1b70117809b384836
hannahlrhl <hannahlarissahalim@gmail.com>
2025-11-20 21:12:55 +0700

    update next.config.mjs

--- fteat_uas_frontend/next.config.mjs
+++ fteat_uas_frontend/next.config.mjs
@@ -1,7 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
+
