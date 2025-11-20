46784c77078071a377414b464d08b0785a3bdf99
hannahlrhl <hannahlarissahalim@gmail.com>
2025-11-20 21:02:38 +0700

    update app/page.js

--- fteat_uas_frontend/src/app/page.js
+++ fteat_uas_frontend/src/app/page.js
@@ -1,13 +1,19 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// "use client";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+// export default function Page() {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     router.replace("/home"); // langsung redirect ke /home
+//   }, [router]);
+
+//   return null; // gak tampil apa-apa di root
+// }
+
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/home"); // langsung redirect ke /home
-  }, [router]);
-
-  return null; // gak tampil apa-apa di root
+  redirect("/home");
 }
