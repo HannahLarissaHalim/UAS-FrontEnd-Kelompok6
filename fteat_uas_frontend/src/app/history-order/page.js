46c89c66e87fa87bef48adc3c22ccc98b33533f1
hannahlrhl <hannahlarissahalim@gmail.com>
2025-12-04 19:16:37 +0700

    Update page.js

--- fteat_uas_frontend/src/app/history-order/page.js
+++ fteat_uas_frontend/src/app/history-order/page.js
@@ -24,6 +24,7 @@ export default function HistoryOrderPage() {
       return;
     }
     const parsedUser = JSON.parse(userData);
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setUser(parsedUser);
     
     const fetchOrders = async () => {
